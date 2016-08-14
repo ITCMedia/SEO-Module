30fad1e901eb1874d29fb1e05cb9d3b9b7c33e34
Роман <byRonni3@yandex.ru>
2016-08-14 20:34:49 +0300

    Версия 1.2.5
    Версия 1.2.5
    24.11.2015
    Обновление главной страницы, корневых директорий, информационных систем,
    магазина
    Добавлена проверка на удаленные записи

--- class.php
+++ class.php
@@ -1,12 +1,13 @@
 <?php
-// Версия 1.2.4
-// 23.11.2015
+// Версия 1.2.5
+// 24.11.2015
 // Обновление главной страницы, корневых директорий, информационных систем, магазина
+// Добавлена проверка на удаленные записи
 class Updater{
 	public $informationsystem_id, $shop_id, $structure_id;
 	
 	public function getStructure($url_level_1){
-		$query_selection = mysql_query("SELECT `id` FROM `structures` WHERE `path` ='{$url_level_1}'");
+		$query_selection = mysql_query("SELECT `id` FROM `structures` WHERE `path` ='{$url_level_1}' AND `deleted` ='0'");
 		$result = mysql_fetch_array($query_selection); // Преобразование выборки в ассоциативный массив для дальнейшего взятия ячейки.
 		$structure_id = $result['id'];
 		//echo '<br>ID Структуры: ' .$structure_id;
@@ -14,7 +15,7 @@ class Updater{
 	}
 	
 	public function updateStructure($deepLevel, $title, $description, $keywords){
-		$result = mysql_query ("UPDATE `structures` SET `seo_title`='{$title}', `seo_description`='{$description}', `seo_keywords`='{$keywords}'  WHERE `path` ='{$deepLevel}'");
+		$result = mysql_query ("UPDATE `structures` SET `seo_title`='{$title}', `seo_description`='{$description}', `seo_keywords`='{$keywords}'  WHERE `path` ='{$deepLevel}' AND `deleted` ='0'");
 		if ($result == 'true')
 		{
 			//echo "<br>Данные для корневого узла " . $deepLevel . " успешно обновлены.";
@@ -22,7 +23,7 @@ class Updater{
 	}
 	
 	public function updateFirstLevel($url_level_1, $title, $description, $keywords){
-		$result = mysql_query ("UPDATE `structures` SET `seo_title`='{$title}', `seo_description`='{$description}', `seo_keywords`='{$keywords}'  WHERE `path` ='{$url_level_1}'");
+		$result = mysql_query ("UPDATE `structures` SET `seo_title`='{$title}', `seo_description`='{$description}', `seo_keywords`='{$keywords}'  WHERE `path` ='{$url_level_1}' AND `deleted` ='0'");
 		if ($result == 'true')
 		{
 			//echo "<br>Данные для корневого узла " . $url_level_1 . " успешно обновлены.";
@@ -30,16 +31,16 @@ class Updater{
 	}
 	
 	public function updateMainpage($title, $description, $keywords){
-		$result = mysql_query ("UPDATE `structures` SET `seo_title`='{$title}', `seo_description`='{$description}', `seo_keywords`='{$keywords}'  WHERE `path` ='/'");
+		$result = mysql_query ("UPDATE `structures` SET `seo_title`='{$title}', `seo_description`='{$description}', `seo_keywords`='{$keywords}'  WHERE `path` ='/' AND `deleted` ='0'");
 	}
 	
 	public function checkSystem($current_structure){
 		$systemReturn = array();
-		$query_selection = mysql_query("SELECT `id` FROM `informationsystems` WHERE `structure_id` ='{$current_structure}'");
+		$query_selection = mysql_query("SELECT `id` FROM `informationsystems` WHERE `structure_id` ='{$current_structure}' AND `deleted` ='0'");
 		$result = mysql_fetch_array($query_selection);
 		$systemReturn['is_id'] = $result['id'];
 		if($systemReturn['is_id'] == ''){
-			$query_selection = mysql_query("SELECT `id` FROM `shops` WHERE `structure_id` ='{$current_structure}'");
+			$query_selection = mysql_query("SELECT `id` FROM `shops` WHERE `structure_id` ='{$current_structure}' AND `deleted` ='0'");
 			$result = mysql_fetch_array($query_selection); 
 			$systemReturn['shop_id'] = $result['id'];
 			//echo '<br>ID Магазин: ' .$systemReturn['shop_id'];
@@ -50,17 +51,17 @@ class Updater{
 	}
 	
 	public function shopUpdate($deepLevel, $shop_id, $title, $description, $keywords){
-		$query_item = mysql_query("SELECT `shop_group_id` FROM `shop_items` WHERE `path` ='{$deepLevel}' AND `shop_id` ='{$shop_id}'");
+		$query_item = mysql_query("SELECT `shop_group_id` FROM `shop_items` WHERE `path` ='{$deepLevel}' AND `shop_id` ='{$shop_id}' AND `deleted` ='0'");
 		$output = mysql_fetch_array($query_item); 
 		$shop_group_id = $output['shop_group_id'];
 		if($shop_group_id != ''){
-			$result = mysql_query ("UPDATE `shop_items` SET `seo_title`='{$title}', `seo_description`='{$description}', `seo_keywords`='{$keywords}'  WHERE `path` ='{$deepLevel}' AND `shop_id` ='{$shop_id}'");
+			$result = mysql_query ("UPDATE `shop_items` SET `seo_title`='{$title}', `seo_description`='{$description}', `seo_keywords`='{$keywords}'  WHERE `path` ='{$deepLevel}' AND `shop_id` ='{$shop_id}' AND `deleted` ='0'");
 			if ($result == 'true')
 			{
 				//echo "<br>Данные для товара " . $deepLevel . " успешно обновлены.";
 			}
 		}else{
-			$result = mysql_query ("UPDATE `shop_groups` SET `seo_title`='{$title}', `seo_description`='{$description}', `seo_keywords`='{$keywords}'  WHERE `path` ='{$deepLevel}' AND `shop_id` ='{$shop_id}'");
+			$result = mysql_query ("UPDATE `shop_groups` SET `seo_title`='{$title}', `seo_description`='{$description}', `seo_keywords`='{$keywords}'  WHERE `path` ='{$deepLevel}' AND `shop_id` ='{$shop_id}' AND `deleted` ='0'");
 			if ($result == 'true')
 			{
 				//echo "<br>Данные для группы магазина " . $deepLevel . " обновлены.";
@@ -69,17 +70,17 @@ class Updater{
 	}
 	
 	public function isUpdate($deepLevel, $informationsystem_id, $title, $description, $keywords){
-		$query_item = mysql_query("SELECT `informationsystem_group_id` FROM `informationsystem_items` WHERE `path` ='{$deepLevel}' AND `informationsystem_id` ='{$informationsystem_id}'");
+		$query_item = mysql_query("SELECT `informationsystem_group_id` FROM `informationsystem_items` WHERE `path` ='{$deepLevel}' AND `informationsystem_id` ='{$informationsystem_id}' AND `deleted` ='0'");
 		$output = mysql_fetch_array($query_item); // Преобразование выборки в ассоциативный массив для дальнейшего взятия ячейки.
 		$informationsystem_group_id = $output['informationsystem_group_id'];
 		if($informationsystem_group_id != ''){
-			$result = mysql_query ("UPDATE `informationsystem_items` SET `seo_title`='{$title}', `seo_description`='{$description}', `seo_keywords`='{$keywords}'  WHERE `path` ='{$deepLevel}' AND `informationsystem_id` ='{$informationsystem_id}'");
+			$result = mysql_query ("UPDATE `informationsystem_items` SET `seo_title`='{$title}', `seo_description`='{$description}', `seo_keywords`='{$keywords}'  WHERE `path` ='{$deepLevel}' AND `informationsystem_id` ='{$informationsystem_id}' AND `deleted` ='0'");
 			if ($result == 'true')
 			{
 				//echo "<br>Данные для инфоэлемента " . $deepLevel . " успешно обновлены.";
 			}
 		}else{
-			$result = mysql_query ("UPDATE `informationsystem_groups` SET `seo_title`='{$title}', `seo_description`='{$description}', `seo_keywords`='{$keywords}'  WHERE `path` ='{$deepLevel}' AND `informationsystem_id` ='{$informationsystem_id}'");
+			$result = mysql_query ("UPDATE `informationsystem_groups` SET `seo_title`='{$title}', `seo_description`='{$description}', `seo_keywords`='{$keywords}'  WHERE `path` ='{$deepLevel}' AND `informationsystem_id` ='{$informationsystem_id}' AND `deleted` ='0' AND `deleted` ='0'");
 			if ($result == 'true')
 			{
 				//echo "<br>Данные для группы " . $deepLevel . " обновлены.";
